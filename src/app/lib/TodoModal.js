de1aa6d9ddc59129222963e1364881e0dd553246
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-10-25 19:11:53 +0500

    trying to populate user

--- src/app/lib/TodoModal.js
+++ src/app/lib/TodoModal.js
@@ -3,5 +3,6 @@ const { Schema, default: mongoose } = require("mongoose");
 const TodoSchema = new Schema({
   task: String,
   isCompleted: Boolean,
+  user : {type : mongoose.Types.ObjectId , ref : "users"}
 });
 export const TodoModal = mongoose.models.todos || mongoose.model("todos", TodoSchema);
